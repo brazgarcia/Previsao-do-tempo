2b45300434cc99315ca97f82a74b4244cbef2fd5
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2025-08-05 22:14:29 -0300

    update css code

--- components/WeatherInformations/WeatherInformations.jsx
+++ components/WeatherInformations/WeatherInformations.jsx
@@ -1,12 +1,24 @@
-function WeatherInformations({weather}) {
+function WeatherInformations( {weather} ) {
+  console.log(weather);
+  return (
+    <div>
+      <h2>{weather.name}</h2>
+      <div>
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt=""
+        />
 
-    console.log(weather);
-    return (
-        <div>
-            <h2>{weather.name}</h2>
-        </div>
-    )
+        <p>{Math.round(weather.main.temp)}ºC</p>
+      </div>
 
+      <p>{weather.weather[0].description}</p>
+      <p>Sensação térmica: {Math.round(weather.main.fells_like)}</p>
+      <p>Umidade: {weather.main.humidity}%</p>
+      <p>Pressão: {weather.main.pressure} hPa</p>
+      
+    </div>
+  );
 }
 
 export default WeatherInformations;
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import WeatherInformations from "../components/WeatherInformations/WeatherInformations";
 
 function App() {
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState();
   const inputRef = useRef();
 
   async function searchCity() {
@@ -15,16 +15,15 @@ function App() {
     const apiInfo = await axios.get(url);
     setWeather(apiInfo.data);
 
-
   }
 
   return (
-    <div>
+    <div className="container">
       <h1>Previsão do tempo</h1>
       <input ref={inputRef} type="text" placeholder="Digite o nome da cidade" />
       <button onClick={searchCity}>Buscar</button>
 
-      <WeatherInformations weather = {weather}/>
+      {weather &&<WeatherInformations weather = {weather}/>}
 
     </div>
   );
